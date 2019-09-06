cae54d83321c4068b818e2a28094bd9afac01847
Izac Lorimer <17418611+theGordHoard@users.noreply.github.com>
2019-09-06 16:05:57 -0600

    Add Season Two assets and asset filtering (#3)
    * Formatting and CSS to external file
    
    * Season Two assets and Asset filtering
    
    * Correct to Arena colours

---
+++ filter.js
@@ -0,0 +1,29 @@
+const assetSections = document.getElementsByClassName('asset-section')
+let assets = []
+
+for (let i = 0; i < assetSections.length; i++) {
+  assets.push(assetSections[i])
+}
+
+let seasonSelection = 's2'
+
+const seasonRadio = document.getElementsByName('season')
+for (let i = 0; i < seasonRadio.length; i++) {
+  seasonRadio[i].onchange = () => {
+    console.log('change')
+    updateFilter(seasonRadio[i].value)
+  }
+}
+
+const updateFilter = (season = seasonSelection) => {
+  seasonSelection = season
+  assets.forEach(asset => {
+    if (seasonSelection === 'all' || !asset.dataset.season || asset.dataset.season === seasonSelection) {
+      asset.classList.add('show')
+    } else {
+      asset.classList.remove('show')
+    }
+  })
+}
+
+updateFilter()
